6aca01484ca1704fc4903ae4dabdeecd1c636909
CallMeRZIBI <michalenko.jakub@gmail.com>
2023-06-01 17:14:19 +0200

    added bounding boxes

--- back_bumper/convert_dataset_to_coco.py
+++ back_bumper/convert_dataset_to_coco.py
@@ -23,12 +23,33 @@ def load_annotation(path):
     ann = json.load(f)
     return ann
 
+def get_bbox(annotation):
+    for ann in annotation["shapes"]:
+        if ann["shape_type"] == "rectangle":
+            bbox = np.array(ann["points"]).flatten()
+
+            # Check if points are in order x_min, y_min, x_max, y_max
+            if bbox[0] > bbox[2]:
+                temp = bbox[0]
+                bbox[0] = bbox[2]
+                bbox[2] = temp
+            if bbox[1] > bbox[3]:
+                temp = bbox[1]
+                bbox[3] = bbox[1]
+                bbox[1] = temp
+
+            bbox = [int(axis) for axis in bbox]
+
+    bbox = np.array(bbox)
+    bbox = np.expand_dims(bbox, axis = 0)
+    return bbox
+
 def get_keypoints(annotation):
     keypoints = []
 
-    for i in range(len(annotation["shapes"])):
-        if annotation["shapes"][i]["shape_type"] == "point":
-            point = np.array(annotation["shapes"][i]["points"])
+    for ann in annotation["shapes"]:
+        if ann["shape_type"] == "point":
+            point = np.array(ann["points"])
             point = np.append(point, 1)
             point = np.squeeze(point.tolist())
             keypoints.append(point)
@@ -40,12 +61,13 @@ def get_keypoints(annotation):
         keypoints[i][1] = int(keypoints[i][1])
         keypoints[i][2] = int(keypoints[i][2])
 
-    keypoints = np.array(keypoints)
     keypoints = np.expand_dims(keypoints, axis = 0)
     return keypoints
 
 def create_annotations(images, path):
-# Looping through all training images to create theirs individual annotations
+    missings = []
+
+    # Looping through all training images to create theirs individual annotations
     for img in images:
         img_name = img[:img.rfind(".")]
 
@@ -56,8 +78,10 @@ def create_annotations(images, path):
 
             if ann_short_name == img_name:
                 keypoints = get_keypoints(ann)
+                bbox = get_bbox(ann)
 
                 annotation = {
+                    "bboxes" : bbox.tolist(),
                     "keypoints" : keypoints.tolist()
                 }
                 annotation = json.dumps(annotation)
@@ -65,6 +89,19 @@ def create_annotations(images, path):
                 save = open(join(path, "annotations", annName), "w")
                 save.write(annotation)
                 save.close()
+                break
+        else:
+            missings.append(ann_short_name)
+
+    new_list = []
+    dup_list = []
+    for i in missings:
+        if i not in new_list:
+            new_list.append(i)
+        else:
+            dup_list.append(i)
+    if len(dup_list) != 0:
+        print(f"{dup_list} are not annotated")
 
 create_annotations(train_imgs, TRAIN_PATH)
 create_annotations(test_imgs, TEST_PATH)
